242d6bb4c7d021fa4425c64c133bc7695041b4b4
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-04-17 14:08:32 -0500

    hit circles and stuff

--- src/beatmap-renderer.ts
+++ src/beatmap-renderer.ts
@@ -1,30 +1,62 @@
 import type { CanvasRenderingContext2D } from "canvas"
+import type { HittableObject } from "osu-parsers"
 import type { Beatmap } from "./download-beatmaps"
 import { lerpClamped } from "./helpers/math"
 
-type Tween = {
+type Animations = {
   start: number
   end: number
   render: (context: CanvasRenderingContext2D, progress: number) => void
 }
 
-const tweens: Tween[] = [
-  // fade in
+const animations = (object: HittableObject): Animations[] => [
+  // circle fading in
   {
     start: -0.5,
     end: 0,
-    render: (context: CanvasRenderingContext2D, progress: number) => {
+    render: (context, progress) => {
       context.globalAlpha = progress
+      context.fillStyle = "cornflowerblue"
+
+      context.beginPath()
+      context.arc(0, 0, 32, 0, 2 * Math.PI)
+      context.fill()
     },
   },
-  // quick fade out and scale up
+
+  // approach circle
+  {
+    start: -0.42,
+    end: 0,
+    render: (context, progress) => {
+      const approachCircleScale = lerpClamped(4, 1, progress)
+
+      context.scale(approachCircleScale, approachCircleScale)
+
+      context.strokeStyle = "cornflowerblue"
+      context.lineWidth = 3
+      context.globalAlpha = progress
+
+      context.beginPath()
+      context.arc(0, 0, 32, 0, 2 * Math.PI)
+      context.stroke()
+    },
+  },
+
+  // hit explosion
   {
     start: 0,
     end: 0.1,
-    render: (context: CanvasRenderingContext2D, progress: number) => {
-      const scale = lerpClamped(1, 1.2, progress)
-      context.globalAlpha = 1 - progress
+    render: (context, progress) => {
+      const scale = lerpClamped(1, 1.5, progress)
       context.scale(scale, scale)
+
+      context.fillStyle = "cornflowerblue"
+      context.globalAlpha = 1 - progress
+
+      context.beginPath()
+      context.arc(0, 0, 32, 0, 2 * Math.PI)
+      context.fill()
     },
   },
 ]
@@ -41,34 +73,27 @@ export function createBeatmapRenderer(beatmap: Beatmap) {
     context.fillStyle = "black"
     context.fillRect(0, 0, context.canvas.width, context.canvas.height)
 
-    context.fillStyle = "cornflowerblue"
-
     for (const object of hitObjects) {
       const startTimeSeconds = object.startTime / 1000
       const relativeTimeSeconds = timeSeconds - startTimeSeconds
 
-      const currentTween = tweens.find(
+      const currentAnimations = animations(object).filter(
         (tween) =>
-          relativeTimeSeconds >= tween.start &&
-          relativeTimeSeconds <= tween.end,
+          relativeTimeSeconds >= tween.start && relativeTimeSeconds < tween.end,
       )
 
-      if (!currentTween) continue
+      for (const animation of currentAnimations) {
+        const progress =
+          (relativeTimeSeconds - animation.start) /
+          (animation.end - animation.start)
 
-      const tweenProgress =
-        (relativeTimeSeconds - currentTween.start) /
-        (currentTween.end - currentTween.start)
+        context.save()
+        context.translate(object.startX, object.startY)
 
-      context.save()
-      context.translate(object.startX, object.startY)
+        animation.render(context, progress)
 
-      currentTween.render(context, tweenProgress)
-
-      context.beginPath()
-      context.arc(0, 0, 32, 0, 2 * Math.PI)
-      context.fill()
-
-      context.restore()
+        context.restore()
+      }
     }
   }
 }
